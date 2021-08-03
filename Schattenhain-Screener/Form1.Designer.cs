07f400d48fac944da327cdff52d74b278190704f
Phillip Weitkamp <pweitkamp@outlook.de>
2021-08-03 18:29:27 +0200

    Add project files.

---
+++ Schattenhain-Screener/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace Schattenhain_Screener
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbSpeicherort = new System.Windows.Forms.TextBox();
+            this.tbIntervall = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbProc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblversion = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbSpeicherort
+            // 
+            this.tbSpeicherort.Location = new System.Drawing.Point(12, 189);
+            this.tbSpeicherort.Name = "tbSpeicherort";
+            this.tbSpeicherort.PlaceholderText = "Klicken zum Einstellen";
+            this.tbSpeicherort.Size = new System.Drawing.Size(278, 23);
+            this.tbSpeicherort.TabIndex = 10;
+            this.tbSpeicherort.Click += new System.EventHandler(this.TbSpeicherort_Click);
+            this.tbSpeicherort.TextChanged += new System.EventHandler(this.TbSpeicherort_TextChanged);
+            // 
+            // tbIntervall
+            // 
+            this.tbIntervall.Location = new System.Drawing.Point(207, 218);
+            this.tbIntervall.Name = "tbIntervall";
+            this.tbIntervall.PlaceholderText = "Empfohlen: 30";
+            this.tbIntervall.Size = new System.Drawing.Size(83, 23);
+            this.tbIntervall.TabIndex = 2;
+            this.tbIntervall.Text = "30";
+            this.tbIntervall.TextChanged += new System.EventHandler(this.TbIntervall_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Speicherort:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Intervall in Sekunden:";
+            // 
+            // tbProc
+            // 
+            this.tbProc.Location = new System.Drawing.Point(135, 145);
+            this.tbProc.Name = "tbProc";
+            this.tbProc.PlaceholderText = "Standart: Schattenhain";
+            this.tbProc.Size = new System.Drawing.Size(155, 23);
+            this.tbProc.TabIndex = 5;
+            this.tbProc.Text = "Schattenhain";
+            this.tbProc.TextChanged += new System.EventHandler(this.TbProc_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Prozess Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(269, 75);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ACHTUNG! Das Programm,\r\nmacht einen Screenshot vom Fenster und allem\r\nwas vor dem" +
+    " Fenster liegt.\r\nStelle sicher das dass Fenster im vordergrund liegt\r\nund nicht " +
+    "verdeckt wird.";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 247);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(278, 23);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Starte / Stoppe Schnüffeln";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Location = new System.Drawing.Point(49, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Bei Fragen/Problemen > Klick mich <";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
+            this.label6.Location = new System.Drawing.Point(6, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(291, 60);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Das Programm ist besten Wissens und Gewissens\r\nProgrammiert worden. Trotzdem kann" +
+    " es zu \r\nFehlern kommen. Solltet ihr also wichtige Screenshots\r\nhaben, speichert" +
+    " sie zusätzlich selbst ab. ";
+            // 
+            // lblversion
+            // 
+            this.lblversion.AutoSize = true;
+            this.lblversion.Location = new System.Drawing.Point(60, 305);
+            this.lblversion.Name = "lblversion";
+            this.lblversion.Size = new System.Drawing.Size(22, 15);
+            this.lblversion.TabIndex = 13;
+            this.lblversion.Text = "1.0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 305);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Version:";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(88, 301);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(202, 23);
+            this.btnDownload.TabIndex = 15;
+            this.btnDownload.Text = "Download Update";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Visible = false;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 329);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblversion);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbProc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbIntervall);
+            this.Controls.Add(this.tbSpeicherort);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Schattenhain Schnüffler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSpeicherort;
+        private System.Windows.Forms.TextBox tbIntervall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbProc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblversion;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnDownload;
+    }
+}
+
---
+++ Schattenhain-Screener/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schattenhain_Screener
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblversion.Text = "1.0";
+            tbSpeicherort.Text = Properties.Settings.Default.SavePath;
+            tbIntervall.Text = Properties.Settings.Default.Intervall;
+            timer1.Interval = Convert.ToInt32(tbIntervall.Text) + 1000;
+            btnStart.BackColor = Color.Red;
+            tbProc.Text = Properties.Settings.Default.ProcName;
+
+            try
+            {
+                WebClient wClient = new WebClient();
+                string strSource = wClient.DownloadString("https://shs.fewiel.de/version.txt");
+                if (strSource != "1.0")
+                {
+                    lblversion.BackColor = Color.Red;
+                    btnDownload.Visible = true;
+                    MessageBox.Show("Update Verfügbar: " + strSource);
+                }
+            }
+            catch (Exception)
+            {
+                lblversion.Text = "Updateserver nicht erreichbar.";
+            }
+        }
+
+        public void CaptureApplication(string procName)
+        {
+            var proc = Process.GetProcessesByName(procName)[0];
+            var rect = new User32.Rect();
+            User32.GetWindowRect(proc.MainWindowHandle, ref rect);
+
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+            try
+            {
+                var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                using (Graphics graphics = Graphics.FromImage(bmp))
+                {
+                    graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+                }
+                var savepath = Path.Combine(tbSpeicherort.Text, DateTime.Now.ToString("dd_MM_yyyy"));
+                if (!Directory.Exists(savepath))
+                {
+                    Directory.CreateDirectory(savepath);
+                }
+                var fullPath = Path.Combine(savepath, tbProc.Text + " " + DateTime.Now.ToString("dd_MM_yyyy HH-mm-ss") + ".jpeg");
+                bmp.Save(@fullPath, ImageFormat.Jpeg);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Rect
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+        }
+
+        private void TbSpeicherort_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbSpeicherort.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void TbSpeicherort_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.SavePath = tbSpeicherort.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void TbIntervall_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Intervall = tbIntervall.Text;
+            Properties.Settings.Default.Save();
+            timer1.Interval = Convert.ToInt32(tbIntervall.Text) + 1000;
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (tbSpeicherort.Text?.Length == 0)
+            {
+                MessageBox.Show("Speicherort festlegen!");
+                return;
+            }
+            if (Convert.ToInt32(tbIntervall.Text) < 10)
+            {
+                MessageBox.Show("Ein Intervall von unter 10 Sek wird nicht empfohlen.");
+                return;
+            }
+            timer1.Interval = Convert.ToInt32(tbIntervall.Text) * 1000;
+            if (!timer1.Enabled)
+            {
+                btnStart.BackColor = Color.Green;
+                timer1.Start();
+                return;
+            }
+            timer1.Stop();
+            btnStart.BackColor = Color.Red;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (Process.GetProcessesByName(tbProc.Text) == null || Process.GetProcessesByName(tbProc.Text).Length == 0)
+            {
+                timer1.Stop();
+                btnStart.BackColor = Color.Red;
+                MessageBox.Show("Schattenhain beendet. Schnüffler Gestoppt!");
+                return;
+            }
+            CaptureApplication(tbProc.Text);
+            CreateZip();
+        }
+
+        private void TbProc_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ProcName = tbProc.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void CreateZip()
+        {
+            var archivPath = Path.Combine(tbSpeicherort.Text, "archiv");
+
+            if (!Directory.Exists(Path.Combine(tbSpeicherort.Text, "archiv")))
+                Directory.CreateDirectory(Path.Combine(tbSpeicherort.Text, "archiv"));
+
+
+            foreach (var d in Directory.GetDirectories(tbSpeicherort.Text))
+            {
+                DirectoryInfo dir = new DirectoryInfo(d);
+                String dirName = dir.Name;
+                if (dirName.Contains("archiv")) continue;
+                var newArchivPath = Path.Combine(archivPath, dirName + ".zip");
+
+                if (Directory.GetCreationTime(d) < DateTime.Now.AddDays(-1))
+                {
+                    ZipFile.CreateFromDirectory(d, newArchivPath, CompressionLevel.Optimal, true);
+                    Directory.Delete(d, true);
+                }
+            }
+
+            foreach (var f in Directory.GetFiles(archivPath))
+            {
+                DateTime dateString = File.GetCreationTime(f);
+                if (dateString < DateTime.Now.AddDays(-14))
+                {
+                    File.Delete(f);
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kontakt auf Schattenhain Discord: Fewiel" + Environment.NewLine + "Email-Kontat: info@panther-host.de");
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            WebClient wClient = new WebClient(); 
+            string target = wClient.DownloadString("https://shs.fewiel.de/download.txt");
+            try
+            {
+                Process.Start(new ProcessStartInfo(target) { UseShellExecute = true });
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }
+    }
+}
